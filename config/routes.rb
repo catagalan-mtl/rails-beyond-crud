ce5665bd672579e6e54a3af76f68e0129dc6bb99
Catalina Galan <catalinagalanvargas@gmail.com>
2023-12-11 15:36:40 -0500

    top and chef added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  resources :restaurants do
+    collection do
+      get :top
+    end
 
+    member do
+      get :chef
+    end
+  end
 end
